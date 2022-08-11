dace882deb31fb6ad586c0bee0ce394bdfd250ba
viniazvd <viniazvd@gmail.com>
2022-08-11 11:59:22 -0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue2'
+import { createVuePlugin } from 'vite-plugin-vue2'
 
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
   plugins: [
-    Vue(),
+    createVuePlugin(),
     Components({ resolvers: [ IconsResolver() ] }),
     Icons({ compiler: 'vue2' })
   ]
